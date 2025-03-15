ac7285cbbf281739994c3ab6da86df41e55c2139
jungsunbeen <tjsqls041103@gmail.com>
2025-03-15 22:01:26 +0900

    init : 선빈 초기 설정

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export const TheHeader = () => {
   return (
-    <div>🐉LUCAUS🐉</div>
+    <Header>LUCAUS</Header>
   )
 }
+
+
+const Header = styled.div`
+  background-color: #1447E6;
+  color: white;
+  text-align: center;
+  // height: 10%;
+`
