bfdbbaa5dc38b2a824077230b7843bcf0d4f1fff
jungsunbeen <tjsqls041103@gmail.com>
2025-03-19 12:05:16 +0900

    design : icon 추가

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -6,7 +6,9 @@ export const TheHeader = () => {
 
   return (
     <Header onClick={() => navigate('/')}>
-      <img src="./images/home/lucaus.png" alt="logo" />
+      <Icon className="left-icon"></Icon>
+      <img style={{ height: '24px' }} src="./images/home/lucaus.png" alt="logo" />
+      <Icon></Icon>
     </Header>
   );
 };
@@ -15,6 +17,23 @@ const Header = styled.div`
   background-color: #1447e6;
   color: white;
   text-align: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   height: 10%;
-  padding: 10%;
+  padding: 0 3%;
+`;
+
+const Icon = styled.div`
+  background-size: cover;
+  width: 30px;
+  height: 30px;
+
+  &.left-icon {
+    background-image: url('./images/home/icon-L.png');
+  }
+
+  &:not(.left-icon) {
+    background-image: none;
+  }
 `;
