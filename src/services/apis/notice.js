48bddc0248a1d8aad8b141e9c9c2a756f2849222
jungsunbeen <tjsqls041103@gmail.com>
2025-03-24 16:07:21 +0900

    fix: 공지 API의 getshortNotice 함수 주석 해제

--- src/services/apis/notice.js
+++ src/services/apis/notice.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 
 const token = localStorage.getItem('token');
 
-// export const getshortNotice = async () => {
-//   try {
-//     const response = await axios.get(`${URL}/api/short-notices`);
-//     return response.data;
-//   } catch (error) {
-//     console.error("Failed to fetch all snowman data", error);
-//     throw error;
-//   }
-// };
+export const getshortNotice = async () => {
+  try {
+    const response = await axios.get(`${URL}/api/short-notices`);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch all snowman data", error);
+    throw error;
+  }
+};
