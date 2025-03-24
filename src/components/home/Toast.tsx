e0cfc1cf8dbe1084c3c8427e2a6bc03347c32cc8
jungsunbeen <tjsqls041103@gmail.com>
2025-03-24 15:43:06 +0900

    style : Toast 컴포넌트 수정 및 정보 아이콘 추가, 이미지 reNaming

--- src/components/home/Toast.tsx
+++ src/components/home/Toast.tsx
@@ -3,9 +3,11 @@ import { Icon } from '../common/GoBackButton';
 
 const Toast = () => {
   return (
-  <Container>
-    <Icon src="/images/common/back.webp" alt="닫기" />
-  </Container>
+    <Container>
+      <Icon src="/images/common/info.webp" alt="information" />
+      <div>텍스트</div>
+      <Icon src="/images/common/refresh.webp" alt="새로고침" />
+    </Container>
   );
 };
 export default Toast;
@@ -13,7 +15,7 @@ export default Toast;
 const Container = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   background-color: #f3f4f6;
   padding: 12px;
