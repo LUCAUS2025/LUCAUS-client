e8faf8608522500d51c8dfdf0bb6ecf79a6efaf5
jungsunbeen <tjsqls041103@gmail.com>
2025-03-24 17:32:31 +0900

    add : 분실물 페이지 추가

---
+++ src/routes/pages/information/lostItem/index.tsx
@@ -0,0 +1,9 @@
+const LostItem = () => {
+    return (
+        <div>
+        <h1>Lost Item</h1>
+        <p>This is the Lost Item page.</p>
+        </div>
+    );
+    }
+export default LostItem;
