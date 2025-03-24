b2beaa16cc45b7fdeb52cb2ff945c0301ebecfc8
jungsunbeen <tjsqls041103@gmail.com>
2025-03-24 16:17:51 +0900

    install : npm install axios

--- src/services/apis/notice.js
+++ src/services/apis/notice.js
@@ -4,11 +4,6 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 export const getshortNotice = async () => {
-  try {
-    const response = await axios.get(`${URL}/api/short-notices`);
-    return response.data;
-  } catch (error) {
-    console.error('Failed to fetch all snowman data', error);
-    throw error;
-  }
+  const response = await axios.get(`${URL}/api/short-notices`);
+  return response.data;
 };
