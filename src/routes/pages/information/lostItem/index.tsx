c766bc3187fa905623599039e3864aba59a6b845
jungsunbeen <tjsqls041103@gmail.com>
2025-03-24 20:33:11 +0900

    add : baseURL 추가

--- src/routes/pages/information/lostItem/index.tsx
+++ src/routes/pages/information/lostItem/index.tsx
@@ -18,8 +18,8 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 30vh;
-  font-size: 24px;
+  // height: 30vh;
+  padding: 16px;
   background-color: #f9fafb;
   border-radius: 8px;
 `;
