92d2745d92564454749b0f11cbff59e532b49b1f
jungsunbeen <tjsqls041103@gmail.com>
2025-03-26 16:02:37 +0900

    feat: 분실물 안내 페이지와 공지사항 페이지에서 아이템 컴포넌트 재사용 및 스타일 개선

--- src/routes/pages/information/notice.tsx
+++ src/routes/pages/information/notice.tsx
@@ -1,8 +1,19 @@
+import { Item, ItemDate, ItemDetail, ItemImage, ItemInfo, ItemList, ItemName, Line, Tag } from './lostitem';
+
 const Notice = () => {
   return (
-    <div>
-      <h1>공지사항</h1>
-    </div>
+    <ItemList>
+      <Item>
+        <ItemInfo>
+          <ItemName>포토부스 이용 안?내</ItemName>
+          <ItemDetail>줄 똑바로 좀</ItemDetail>
+          <Line>
+            <Tag>카테고리</Tag>
+            <ItemDate>등록 일시 | 05.22 오후 6:00</ItemDate>
+          </Line>
+        </ItemInfo>
+      </Item>
+    </ItemList>
   );
 };
 export default Notice;
