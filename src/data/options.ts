aab17243e77956e8e7c4afde21f87acf34b3cf71
jungsunbeen <tjsqls041103@gmail.com>
2025-03-29 19:46:18 +0900

    feat: 아이템 옵션 추가

--- src/data/options.ts
+++ src/data/options.ts
@@ -15,3 +15,12 @@ export const placeOptions: Option[] = [
   { label: '해방광장 일대', value: 'freeSquare' },
   { label: '후문 일대', value: 'backDoor' },
 ];
+
+export const itemsOptions: Option[] = [
+  { label: '전체', value: 'TOTAL' },
+  { label: '생필품', value: 'DAILY_NECESSITIES' },
+  { label: '전자기기', value: 'ELECTRONICS' },
+  { label: '의류', value: 'CLOTHING' },
+  { label: '지갑/카드', value: 'WALLET_CARD' },
+  { label: '기타', value: 'OTHERS' },
+];
