9508a7ad8ebadd1f2a9caf3e94600fe616e55928
jungsunbeen <tjsqls041103@gmail.com>
2025-04-06 18:43:14 +0900

    refactor: React import 최적화 - useState 및 useEffect 제거

--- src/components/BottomSheet/ItemDetailComponents/DetailReview.tsx
+++ src/components/BottomSheet/ItemDetailComponents/DetailReview.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { ReviewBarList } from '../../review/ReviewBarList';
 import { ReviewItem } from '../../review/ReviewBarItem';
