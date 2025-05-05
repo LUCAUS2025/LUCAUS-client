77b8a47f68e705e09cd03800005805b852c20efb
hwiy01 <vdocom02@naver.com>
2025-05-06 03:08:44 +0900

    fix: base Layer 모바일 고정

--- src/components/BottomSheet/layout/BaseLayer.tsx
+++ src/components/BottomSheet/layout/BaseLayer.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import styled from 'styled-components';
 
 interface BaseLayerProps {
@@ -7,29 +7,41 @@ interface BaseLayerProps {
 }
 
 export const BaseLayer: React.FC<BaseLayerProps> = ({ children, backgroundImgSrc }) => {
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    setVh();
+    window.addEventListener('resize', setVh);
+    return () => window.removeEventListener('resize', setVh);
+  }, []);
+
   return (
     <Wrapper>
-      <Background $backgroundImgSrc={backgroundImgSrc} />
+      {backgroundImgSrc && <BackgroundImg src={backgroundImgSrc} alt="background" />}
       <Content>{children}</Content>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  position: relative;
-  width: 100%;
-  min-height: 40vh;
-  overflow: hidden;
   position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: calc(var(--vh, 1vh) * 100);
+  overflow: hidden;
+  z-index: 0;
 `;
 
-const Background = styled.div<{ $backgroundImgSrc?: string }>`
+const BackgroundImg = styled.img`
   position: absolute;
   inset: 0;
-  background-image: ${({ $backgroundImgSrc }) => ($backgroundImgSrc ? `url(${$backgroundImgSrc})` : 'none')};
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+  width: 100%;
+  object-fit: contain;
+  z-index: -1;
 
   user-select: none;
   -webkit-user-drag: none;
@@ -40,4 +52,5 @@ const Background = styled.div<{ $backgroundImgSrc?: string }>`
 const Content = styled.div`
   position: relative;
   z-index: 1;
+  height: 100%;
 `;
