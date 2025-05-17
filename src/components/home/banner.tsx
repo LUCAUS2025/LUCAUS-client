460beb0a0177654972c95f5d4b384f457325aae4
jungsunbeen <tjsqls041103@gmail.com>
2025-05-18 00:45:29 +0900

    add : 배너 추가

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -15,11 +15,7 @@ export const Banner = () => {
     '/images/home/banner/1.webp',
     '/images/home/banner/2.webp',
     '/images/home/banner/3.webp',
-    '/images/home/banner/1.webp',
-    '/images/home/banner/2.webp',
-    '/images/home/banner/3.webp',
-    '/images/home/banner/1.webp',
-    '/images/home/banner/2.webp',
+    '/images/home/banner/4.webp',
   ];
 
   const startAutoSlide = () => {
